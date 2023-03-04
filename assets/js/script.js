523d42f13bdec5aee42b5f35b084134e0894d681
SimoneS86 <simone.sensini.86@gmail.com>
2023-03-04 13:47:29 +0100

    nav fix

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('scroll', function() {
+    let navbar = document.querySelector('.navbar');
+    let nav_button = document.querySelector('.nav_button')
+    if (window.pageYOffset > 500) {
+      navbar.style.backgroundColor = "white"
+      nav_button.style.backgroundColor = "#1a8917"
+      navbar.style.transition = 'background-color 1s ease';
+      nav_button.style.transition = 'background-color 1s ease';
+    } else {
+        navbar.style.backgroundColor = "#ffc017"
+        nav_button.style.backgroundColor = "#191919"
+        navbar.style.transition = 'background-color 1s ease';
+        nav_button.style.transition = 'background-color 1s ease';
+    }
+  });
